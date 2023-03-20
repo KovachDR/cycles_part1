ac0ccc47b720880626ed7349171b81559f506807
Brein <Brein_02_93@mail.ru>
2023-03-20 10:17:41 +0300

    ДЗ выполнено.Удалил пустое пространство между строками кода.

--- src/CyclesPartOne.java
+++ src/CyclesPartOne.java
@@ -11,56 +11,48 @@ public class CyclesPartOne {
         task9();
         task10();
     }
-
     public static void task1() {
         System.out.println("Задача №1");
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
     }
-
     public static void task2() {
         System.out.println("Задача №2");
         for (int i = 10; i >= 1; i--) {
             System.out.println(i);
         }
     }
-
     public static void task3() {
         System.out.println("Задача №3");
         for (int i = 0; i <= 17; i = i + 2) {
                 System.out.println(i);
         }
     }
-
     public static void task4() {
         System.out.println("Задача №4");
         for (int i = 10; i >= -10; i--) {
             System.out.println(i);
         }
     }
-
     public static void task5() {
         System.out.println("Задача №5");
         for (int i = 1904; i <= 2096; i = i + 4) {
             System.out.println(i + " год является високосным");
         }
     }
-
     public static void task6() {
         System.out.println("Задача №6");
         for (int i = 7; i <= 98; i = i + 7) {
             System.out.printf(i + " ");
         }
     }
-
     public static void task7() {
         System.out.println("Задача №7");
         for (int i = 1; i <= 512; i = i * 2) {
             System.out.printf(i + " ");
         }
     }
-
     public static void task8() {
         System.out.println("Задача №8");
         int cash = 29000;
@@ -70,7 +62,6 @@ public class CyclesPartOne {
             System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
         }
     }
-
     public static void task9() {
         System.out.println("Задача №9");
         int cash = 29000;
@@ -88,5 +79,4 @@ public class CyclesPartOne {
             System.out.println(a + " * " + i + " = " + b);
         }
     }
-
 }
